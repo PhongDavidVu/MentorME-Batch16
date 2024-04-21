f56a3c19d4678ddcb3bb7c91d01849da6a500a1b
tuanphongv <tuanphongDavid@gmail.com>
2024-04-21 15:55:59 +1000

    W5+6 work

--- hw/lib/main.dart
+++ hw/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'signup/sign_up_screen.dart';
-
+import 'generated/l10n.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -11,9 +12,36 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHome()
+      home: MyHome(),
+      localizationsDelegates: const [
+        Batch16String.delegate,
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: Batch16String.delegate.supportedLocales,
+      theme: ThemeData (
+        colorScheme:  ColorScheme.fromSeed(seedColor: Colors.deepOrange)
+      ),
     );
   }
 }
 
+
+class PrivateMaterialLocalizations extends LocalizationsDelegate<MaterialLocalizations>{
+  @override
+  bool isSupported(Locale locale) {
+    // TODO: implement isSupported
+    return locale.languageCode == 'vi' || locale.languageCode == 'en';
+  }
+
+  @override
+  Future<MaterialLocalizations> load(Locale locale) {
+    // TODO: implement load
+    return SynchronousFuture<MaterialLocalizations> (PrivateMaterialLocalizations() as MaterialLocalizations);
+  }
+
+  @override
+  bool shouldReload(covariant LocalizationsDelegate<MaterialLocalizations> old) => false;
+
+}
 
